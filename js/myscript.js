a15870cf9368bf84b794f0390b34424ebb6d34ca
Miss Terry Davis <tdavis_909@hotmail.com>
2026-05-15 18:34:47 +0100

    Conneted API, data fetch and styled random drink card

--- js/myscript.js
+++ js/myscript.js
@@ -91,3 +91,43 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=m')
             errorMessage.textContent = 'Error fetching drink data.';
         });
 });
+
+const randomBtn = document.getElementById('random-btn');
+const randomDrinkResult = document.getElementById('random-drink-result');
+randomBtn.addEventListener('click', function() {
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+                
+                randomDrinkResult.innerHTML = '';
+                data.drinks.forEach(drink => {
+                    randomDrinkResult.innerHTML += `
+                       <div class="col-12 col-md-6 col-lg-4 mb-4"> 
+                     <div class="card h-100">
+                  <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" 
+                class="card-img-top">
+                     
+                    <div class="card-body">
+                      <h5>${drink.strDrink}</h5>
+                        <p>${drink.strInstructions.slice(0, 200)}...</p>
+                        <p><strong>Glass:</strong> ${drink.strGlass}</p>
+                          <p><strong>Category:</strong> ${drink.strCategory}</p>
+
+                          <p>${drink.strIngredient1}</p>
+                           <p>${drink.strIngredient2}</p>
+                             <p>${drink.strIngredient3}</p>
+                           </div>
+                        </div>
+                    </div>
+                `;
+            });
+        })
+
+
+    .catch(error => {
+            console.error('Error fetching drink data:', error);
+            errorMessage.textContent = 'Error fetching drink data.';
+        });
+});
